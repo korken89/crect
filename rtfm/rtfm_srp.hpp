951116977fd74427d992a102da4fb2290b926bac
Emil Fresk <emil.fresk@gmail.com>
2017-02-11 22:14:21 +0100

    Updated link script and fixed stack

--- rtfm/rtfm_srp.hpp
+++ rtfm/rtfm_srp.hpp
@@ -5,6 +5,13 @@
 
 #include "stm32f411xe.h"
 
+/**
+ * @brief Symbols exported by the linker script:
+ */
+extern uint32_t __preinit_array_start, __preinit_array_end, __init_array_start;
+extern uint32_t __init_array_end, __fini_array_start, __fini_array_end;
+extern uint32_t __text_end, __data_start, __data_end, __bss_start, __bss_end;
+extern uint32_t __all_end, __stack;
 
 /*
  * Place MCU definition file here.
@@ -16,7 +23,7 @@
  * - __F_CPU              Clock speed of the MCU (needed for time conversions)
  */
 
-#define __STACK_END         (1024*8)
+#define __STACK_END         &__stack
 #define __MCU_NUM_VECTORS   SPI5_IRQn
 #define __F_CPU             168000000
 
