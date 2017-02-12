e0c82d981d1fdd92213264676f5da63e13d37581
Emil Fresk <emil.fresk@gmail.com>
2017-02-12 14:33:29 +0100

    Moved ClockInit to init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -69,6 +69,74 @@ void my_exec_array(uint32_t *from, uint32_t *to)
 
 }
 
+__attribute__ ((noinline))
+void InitClocks()
+{
+  RCC->APB1ENR |= RCC_APB1ENR_PWREN;  // Enable power control clocks
+  PWR->CR |= (3 << PWR_CR_VOS_Pos);   // Volate Scale 1 (<= 100 MHz)
+
+  // Set HSE configuration to BYPASS ( 8 MHz from STlink )
+  RCC->CR |= RCC_CR_HSEBYP;
+  RCC->CR |= RCC_CR_HSEON;
+
+  // Wait till HSE is ready
+  while((RCC->CR & RCC_CR_HSERDY) == 0);
+
+  //
+  // PLL startup sequence
+  //
+
+  // Disable PLL
+  RCC->CR &= ~(1 << RCC_CR_PLLON_Pos);
+
+  // Wait till PLL is ready
+  while((RCC->CR & RCC_CR_PLLRDY) != 0);
+
+  // Configure the main PLL clock source, multiplication and division factors.
+  RCC->PLLCFGR = ((1 << RCC_PLLCFGR_PLLSRC_Pos) |  // HSE
+                  (4 << RCC_PLLCFGR_PLLM_Pos)   |  // PLLM
+                  (200 << RCC_PLLCFGR_PLLN_Pos) |  // PLLN
+                  (1 << RCC_PLLCFGR_PLLP_Pos)   |  // PLLP div / 4
+                  (4 << RCC_PLLCFGR_PLLQ_Pos));    // PLLQ
+
+  // Enable the main PLL.
+  RCC->CR |= (1 << RCC_CR_PLLON_Pos);
+
+  // Wait till PLL is ready
+  while((RCC->CR & RCC_CR_PLLRDY) == 0);
+
+
+  //
+  // Clock configuration sequence
+  //
+
+  // Set flash latency (3 WS, taken from STM32CubeMX)
+  FLASH->ACR |= (3 << FLASH_ACR_LATENCY_Pos);
+
+  // Check to it took hold
+  while ((FLASH->ACR & FLASH_ACR_LATENCY) != 3);
+
+
+  // HCLK Configuration
+  RCC->CFGR &= ~RCC_CFGR_HPRE;  // DIV1
+
+
+  // SYSCLK Configuration (select PLL)
+  RCC->CFGR |= RCC_CFGR_SW_PLL;
+
+  // Wait for PLL
+  while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+
+
+  // PCLK1 Configuration
+  RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
+
+
+  // PCLK2 Configuration
+  RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
+
+}
+
 __attribute__((naked))
 void Reset_Handler()
 {
@@ -94,6 +162,9 @@ void Reset_Handler()
 
   // Setup the NVIC
   NVIC_SetPriorityGrouping(0);
+
+  // Setup clocks
+  InitClocks();
 
   __enable_irq();
   __ISB();
