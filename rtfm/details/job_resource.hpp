20be4ddda0ee29e35e83683f8f6c8cc805f1f3f8
Emil Fresk <emil.fresk@gmail.com>
2017-02-16 20:41:31 +0100

    Fixed so priority and UID are unsigned

--- rtfm/details/job_resource.hpp
+++ rtfm/details/job_resource.hpp
@@ -11,7 +11,7 @@ namespace rtfm
 /**
  * @brief Job maximum priority.
  */
-using max_priority = brigand::integral_constant<int,
+using max_priority = brigand::integral_constant<unsigned,
                                                 (1 << __NVIC_PRIO_BITS) - 1>;
 
 /**
@@ -22,11 +22,11 @@ using max_priority = brigand::integral_constant<int,
  * @tparam ISR    ISR definition.
  * @tparam Res    Parameter pack of resources.
  */
-template <int UID_, int Prio_, typename ISR_, typename... Res>
+template <unsigned UID_, unsigned Prio_, typename ISR_, typename... Res>
 struct Job
 {
-  using UID = brigand::integral_constant<int, UID_>;
-  using P = brigand::integral_constant<int, Prio_>;
+  using UID = brigand::integral_constant<unsigned, UID_>;
+  using P = brigand::integral_constant<unsigned, Prio_>;
   using ISR = ISR_;
   using resources = brigand::flatten< brigand::list<Res...> >;
 
