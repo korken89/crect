29f03e0d3ea6d7ad8945f3cea2958124dc6122f2
Emil Fresk <emil.fresk@gmail.com>
2017-02-19 13:40:58 +0100

    Forgot the clock file

---
+++ rtfm/rtfm_clock.hpp
@@ -0,0 +1,66 @@
+
+#pragma once
+
+#include <cstdint>
+#include <chrono>
+
+namespace rtfm
+{
+namespace time
+{
+
+/**
+ * @brief     The definition of the system clock.
+ * @details   It is based on the DWT cycle counter for calculating time, this
+ *            means that system_clock::now() needs to be called atleats once per
+ *            overflow of the DWT, else the time will not be correct.
+ *            It is also possible to replace this function with another timer
+ *            implementation.
+ */
+struct system_clock
+{
+  /** @brief  Definition of the duration of timer ticks running at MCU clock. */
+  using duration = std::chrono::duration<uint64_t, std::ratio<1, __F_CPU>>;
+  using rep = duration::rep;
+  using period = duration::period;
+  using time_point = std::chrono::time_point<system_clock>;
+  static const bool is_steady = false;
+
+  /**
+   * @brief   Extraction of the current time in clock ticks using the DWT.
+   * @note    This function MUST be called more often than the time for an
+   *          overflow of the DWT counter, else the system time will be
+   *          incorrect.
+   *
+   * @return  Returns the current time.
+   */
+  static time_point now() noexcept
+  {
+    /* Holds the current offset from start time due to DWT overflows. */
+    static uint64_t base = 0;
+
+    /* Holds the old DWT value to check for overflows. */
+    static uint32_t old_dwt = 0;
+
+    uint32_t dwt = DWT->CYCCNT;
+
+    /* START CRITICAL SECTION */
+    //__disable_irq();
+    //__ISB(); // Is the ISB needed to make __disable_irq take hold?
+
+    /* If the DWT has overflowed, update the base. */
+    if (old_dwt > dwt)
+      base += 0xffffffff;
+
+    /* Save old DWT. */
+    old_dwt = dwt;
+
+    /* END CRITICAL SECTION */
+    //__enable_irq();
+
+    return time_point(duration(base + dwt));
+  }
+};
+
+} /* END namespace time */
+} /* END namespace rtfm */
