90d3e955c0f0606a881c9d7780686636fb319d30
Emil Fresk <emil.fresk@gmail.com>
2017-02-23 17:53:15 +0100

    ISER, enables an interrupt, not ICER...

--- rtfm/srp/srp_init.hpp
+++ rtfm/srp/srp_init.hpp
@@ -20,7 +20,7 @@ struct job_to_nvic_printer
     using Prio = typename Job::P;
 
     /* Enable interrupt. */
-    NVIC->ICER[ISRn::value >> 5UL] = (1UL << (ISRn::value & 0x1FUL));
+    NVIC->ISER[ISRn::value >> 5UL] = (1UL << (ISRn::value & 0x1FUL));
 
     /* Set priority */
     NVIC->IP[ISRn::value] = util::priority_to_NVIC_priority(Prio::value);
