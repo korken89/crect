7eb3d15a56c37eb259b0b322c92774be4f5159d8
Emil Fresk <emil.fresk@gmail.com>
2017-03-11 13:25:00 +0100

    Updated Makefile and restructured test.cpp
    Added so the makefile will compile using clang and g++ to catch errors
    that are compiler dependent, moreover made the new template additions eager.

--- example/test.cpp
+++ example/test.cpp
@@ -3,17 +3,19 @@
 #include "../../mpl_debug/print_types.hpp"
 #include "kvasir/mpl/mpl.hpp"
 
+namespace details
+{
 template <typename... T>
-struct GetIntegralType;
+struct GetIntegralType_impl;
 
 template <typename T, T val>
-struct GetIntegralType<kvasir::mpl::integral_constant<T, val>>
+struct GetIntegralType_impl<kvasir::mpl::integral_constant<T, val>>
 {
   using f = T;
 };
 
 template <>
-struct GetIntegralType<
+struct GetIntegralType_impl<
     kvasir::mpl::integral_constant<decltype(nullptr), nullptr>>
 {
   using f = decltype(nullptr);
@@ -21,25 +23,57 @@ struct GetIntegralType<
 
 
 
-template <typename T>
-struct IsNullPtr
+template <typename... T>
+struct IsNullPtr_impl;
+
+template <typename T, T val>
+struct IsNullPtr_impl<kvasir::mpl::integral_constant<T, val>>
 {
   using f = kvasir::mpl::bool_<false>;
 };
 
 template <typename T>
-struct IsNullPtr<kvasir::mpl::integral_constant<T, nullptr>>
+struct IsNullPtr_impl<kvasir::mpl::integral_constant<T, nullptr>>
 {
   using f = kvasir::mpl::bool_<true>;
 };
 
 template <>
-struct IsNullPtr<kvasir::mpl::integral_constant<decltype(nullptr), nullptr>>
+struct IsNullPtr_impl<
+    kvasir::mpl::integral_constant<decltype(nullptr), nullptr>>
 {
   using f = kvasir::mpl::bool_<true>;
 };
 
 
+
+template <bool HasObject, typename Fun, typename Object>
+struct claim_impl;
+
+template <typename Fun, typename Object>
+struct claim_impl<true, Fun, Object>
+{
+  auto operator()(Fun &&f) const noexcept
+  {
+    return f( std::ref(*Object{}) ); // Pass by reference
+  }
+};
+
+template <typename Fun, typename Object>
+struct claim_impl<false, Fun, Object>
+{
+  auto operator()(Fun &&f) const noexcept
+  {
+    return f();
+  }
+};
+}
+
+template <typename T>
+using GetIntegralType = typename details::GetIntegralType_impl<T>::f;
+
+template <typename T>
+using IsNullPtr = typename details::IsNullPtr_impl<T>::f;
 
 template <typename ID_, typename Object, bool Unique, typename... Jobs>
 struct Resource
@@ -47,16 +81,31 @@ struct Resource
   static_assert(kvasir::mpl::is_integral<Object>{},
                 "Object must be an integral constant.");
 
-  static_assert((std::is_pointer<typename GetIntegralType<Object>::f>::value ||
-                 typename IsNullPtr<Object>::f{}),
+  static_assert((std::is_pointer<GetIntegralType<Object>>::value ||
+                 IsNullPtr<Object>{}),
                 "The type of the object must be a pointer.");
 
   using ID = ID_;
   using object = Object;
-  using has_object = kvasir::mpl::bool_<!typename IsNullPtr<Object>::f{}>;
+  using has_object = typename kvasir::mpl::bool_<!IsNullPtr<Object>{}>;
   using is_unique = kvasir::mpl::bool_<Unique>;
   using jobs = kvasir::mpl::flatten<kvasir::mpl::list<Jobs...>>;
 };
+
+
+
+template <typename Resource, typename Fun>
+constexpr auto claim(Fun &&f)
+{
+  using object = typename Resource::object;
+  using has_object = typename Resource::has_object;
+
+  // rtfm::lock<Resource> lock;
+
+  return details::claim_impl<has_object{}, Fun, object>{}(std::forward<Fun>(f));
+}
+
+
 
 template <typename ID, typename Object, typename... Jobs>
 using MakeResource = Resource<ID, Object, false, Jobs...>;
@@ -77,37 +126,6 @@ using MakeUniqueVirtualResource =
 template <typename... Resources>
 using MakeResourceAlias = kvasir::mpl::list<Resources...>;
 
-template <bool HasObject, typename Fun, typename Object>
-struct claim_impl;
-
-template <typename Fun, typename Object>
-struct claim_impl<true, Fun, Object>
-{
-  auto operator()(Fun &&f) const noexcept
-  {
-    return f(std::ref(*Object{}));
-  }
-};
-
-template <typename Fun, typename Object>
-struct claim_impl<false, Fun, Object>
-{
-  auto operator()(Fun &&f) const noexcept
-  {
-    return f();
-  }
-};
-
-template <typename Resource, typename Fun>
-constexpr auto claim(Fun &&f)
-{
-  using obj = typename Resource::object;
-
-  // rtfm::lock<Resource> lock;
-
-  return claim_impl<typename Resource::has_object{}, Fun, obj>{}(
-      std::forward<Fun>(f));
-}
 
 int i = 0;
 
@@ -115,30 +133,37 @@ int main()
 {
   using namespace std;
 
-  using tr =
-    MakeResource<int, kvasir::mpl::integral_constant<decltype(&i), &i>>;
-    // MakeVirtualResource<int>;
+  using R1 =
+      MakeResource<int, kvasir::mpl::integral_constant<decltype(&i), &i>>;
+  using R2 = MakeVirtualResource<float>;
 
-
-
-  using nr = Resource<typename tr::ID, typename tr::object, typename tr::is_unique{}>;
+  using nr =
+      Resource<typename R1::ID, typename R1::object, typename R1::is_unique{}>;
+  cout << endl << "R1: " << type_name<R1>() << endl;
   cout << "nr: " << type_name<nr>() << endl << endl;
 
 
-  // Not yet working
-  using ii = kvasir::mpl::is_integral< kvasir::mpl::integral_constant< decltype(nullptr), nullptr > >;
-  //cout << "ii: " << ii{} << endl << endl;
+  cout << "i is now 1: " << i << endl;
 
-  cout << "i is now 1: " << i << endl << endl;
-
-  auto ret = claim<tr>([](int &j) {
+  auto ret = claim<R1>([](int &j) {
     // int j = 0;
     j++;
-    cout << "Hello from lambda... j = " << j << endl << endl;
+    cout << "Hello from claimed resource lambda... j = " << j << endl;
 
     return j + 100;
   });
 
-  cout << "claim return: " << ret << endl << endl;
+  cout << "claim return: " << ret << endl;
   cout << "i is now 2: " << i << endl << endl;
+
+  auto ret2 = claim<R2>([]() {
+    int j = 100;
+    j++;
+    cout << "Hello from virtual resource lambda... j = " << j << endl;
+
+    return j + 100;
+  });
+
+  cout << "claim return: " << ret2 << endl;
+  cout << "i is now 3: " << i << endl << endl;
 }
