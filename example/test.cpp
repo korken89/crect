b970294442942ee40f12d6c7a05a3734b1be8bcf
Emil Fresk <emil.fresk@gmail.com>
2017-03-14 09:12:17 +0100

    Fixing support for auto in lambda

--- example/test.cpp
+++ example/test.cpp
@@ -5,23 +5,30 @@
 #include "rtfm/rtfm_utils.hpp"
 
 
+
 template <typename Fun>
 constexpr auto forward_function(Fun &&f)
 {
   using namespace std;
-  using rtype = rtfm::util::function_traits<Fun>;
 
-  static_assert(typename rtype::n_args{} == 1,
-                "The lambda may only take one argument" );
+  // template<typename... Ts>
+  // using TestType = decltype(f(std::declval<Ts>()...))(Ts...);
 
-  using argtype = typename rtype::template arg<0>;
+  cout << "Function's type: " << type_name< TestType<Fun> >() << "\n";
 
-  static_assert(std::is_reference<argtype>::value,
-                "The lambda may take arguments by reference." );
+  //using rtype = rtfm::util::function_traits<Fun>;
 
-  cout << "Function's type:           " << type_name< rtype >() << "\n";
-  cout << "Function's no. arguments:  " << typename rtype::n_args{} << "\n";
-  cout << "Function's first argument: " << type_name< argtype >() << "\n";
+  //static_assert(typename rtype::n_args{} == 1,
+  //              "The lambda may only take one argument" );
+
+  //using argtype = typename rtype::template arg<0>;
+
+  //static_assert(std::is_reference<argtype>::value,
+  //              "The lambda may take arguments by reference." );
+
+  //cout << "Function's type:           " << type_name< rtype >() << "\n";
+  //cout << "Function's no. arguments:  " << typename rtype::n_args{} << "\n";
+  //cout << "Function's first argument: " << type_name< argtype >() << "\n";
 }
 
 int i = 0;
@@ -38,7 +45,7 @@ int main()
 {
   using namespace std;
 
-  forward_function([](int &i) { return i + 3; });
+  forward_function([](auto &i) { return i + 3; });
 
   cout << "link_object: " << type_name< link_object(&i) >() << "\n";
 }
