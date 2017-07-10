e74f8e1d6ed1434508304f3bd56d705fa707f5fa
Emil Fresk <emil.fresk@gmail.com>
2017-07-10 15:41:47 +0200

    Added license headers

--- src/crect/clock.cpp
+++ src/crect/clock.cpp
@@ -1,5 +1,8 @@
+//          Copyright Emil Fresk 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
-//#include "crect/crect.hpp"
 #include "crect/clock.hpp"
 
 
--- src/crect/details/job_resource_details.hpp
+++ src/crect/details/job_resource_details.hpp
@@ -1,3 +1,7 @@
+//          Copyright Emil Fresk 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
 
